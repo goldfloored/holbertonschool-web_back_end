7f132f6306b4c8dbe82793c9c3dd53ef6bba0071
goldfloored <goldfloored@gmail.com>
2022-08-25 13:26:00 +0100

    com

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -10,33 +10,41 @@ class Auth:
     """
     Authentication Class
     """
+    def __init__(self):
+        pass
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ auth class module docstring for holberton checker
         """
-        Authentication
-        """
-        if path is None:
+        if not path or not excluded_paths or excluded_paths == []:
             return True
-        if excluded_paths is None or excluded_paths == []:
-            return True
-        if path[-1] != "/":
-            path += "/"
-        if path in excluded_paths:
-            return False
-        else:
-            return True
+        if not isinstance(path, str):
+            raise TypeError('path must be a string')
+        for excluded in excluded_paths:
+            if excluded.endswith("*"):
+                excluded = excluded[:-1]
+                if path.startswith(excluded):
+                    return False
+            if path in excluded or path == excluded:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """
-        Authorization
-        """
-        if request is None:
+        """ rauth class module docstring for holberton checker """
+        if request and "Authorization" in request.headers:
+            return request.headers.get("Authorization")
+        else:
             return None
-        if not request.headers.get("Authorization"):
-            return None
-        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-        current user
-        """
+        """ auth class module docstring for holberton checker """
         return None
+
+    def session_cookie(self, request=None):
+        """ auth class module docstring for holberton checker """
+        from os import getenv
+        if request:
+            session_name = getenv("SESSION_NAME")
+            return request.cookies.get(session_name, None)
+        return None
+
--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,92 +1,76 @@
 #!/usr/bin/env python3
+
 """
-basic auth
+Basic Auth class module
 """
+
 import base64
-from email.header import decode_header
-from email.mime import base
-from typing import Tuple
 from api.v1.auth.auth import Auth
 from typing import TypeVar
-from models.user import User
 
 
 class BasicAuth(Auth):
+    """ Basic Auth class function
     """
-    basic Auth
-    """
+    def __init__(self):
+        pass
 
-    def extract_base64_authorization_header(self, authorization_header: str
-                                            ) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """
-        return base64 auth header
+        Basic Auth class function
         """
-        if authorization_header is None:
-            return None
-        elif isinstance(authorization_header, str) is False:
-            return None
-        elif authorization_header[0:6] != "Basic ":
-            return None
-        else:
-            return authorization_header[6:]
+        a = authorization_header
+        if not a or not isinstance(a, str) or not a.startswith("Basic "):
+            return
+        return a[6:]
 
-    def decode_base64_authorization_header(
-        self, base64_authorization_header: str
-    ) -> str:
-        """
-        return decode value utf-8 string
-        """
-        if base64_authorization_header is None:
-            return None
-        if isinstance(base64_authorization_header, str) is False:
+    def decode_base64_authorization_header(self, b64_auth_header: str) -> str:
+        """ Basic Auth class function """
+        auth = b64_auth_header
+        if not isinstance(auth, str) or not auth:
             return None
         try:
-            return base64.b64decode(base64_authorization_header
-                                    ).decode('utf-8')
+            return base64.b64decode(auth.encode('utf-8')).decode('utf-8')
         except Exception:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str):
-        """
-        return user credentials
-        """
-        if decoded_base64_authorization_header is None:
-            return None, None
-        if type(decoded_base64_authorization_header) is not str:
-            return None, None
-        if decoded_base64_authorization_header.__contains__(":") is False:
-            return None, None
-        user_Credentials = decoded_base64_authorization_header.split(":")
-        return (user_Credentials[0], user_Credentials[1])
+    def extract_user_credentials(self, enc_b64_auth_headr: str):
+        """ Basic Auth class function """
+        auth = enc_b64_auth_headr
+        bad_return = (None, None)
+        if not isinstance(auth, str) or not auth or ':' not in auth:
+            return bad_return
+        email = auth.split(':')[0]
+        password = auth.split(':', 1)[1]
+        return (email, password)
 
-    def user_object_from_credentials(
-        self, user_email: str, user_pwd: str
-    ) -> TypeVar('User'):
-        """
-        returns the User instance based on his email and password.
-        """
-        if (type(user_email) is not str or type(user_pwd) is not str):
+    def user_object_from_credentials(self, e: str, p: str) -> TypeVar('User'):
+        """ Basic Auth class module docstring for holberton checker """
+        if not e or not isinstance(e, str):
             return None
+        if not p or not isinstance(p, str):
+            return None
+        # ret None if no user with that email
+        # same if that user's pwd is wrong
         try:
-            user = User.search({"email": user_email})
+            email = {"email": e}
+            from models.user import User
+            user = User.search(email)
         except Exception:
             return None
-        if user[0].is_valid_password(user_pwd) and user:
+        if not user:
+            return None
+        if user[0].is_valid_password(p):
             return user[0]
-        return None
+        else:
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-        return current user
-        """
-        try:
-            header = self.authorization_header(request)
-            base64_auth = self.extract_base64_authorization_header(header)
-            decode_header = self.decode_base64_authorization_header(
-                base64_auth)
-            user_credentials = self.extract_user_credentials(
-                decode_header)
-            return self.user_object_from_credentials(
-                user_credentials[0], user_credentials[1])
-        except Exception:
-            return None
+        """ Basic Auth class module docstring for holberton checker """
+        auth = self.authorization_header(request)
+        base = self.extract_base64_authorization_header(auth)
+        decode = self.decode_base64_authorization_header(base)
+        user = self.extract_user_credentials(decode)
+        return self.user_object_from_credentials(user[0], user[1])
+
