f1573643b057790dc2475e65d1971d6fba791bdb
goldfloored <goldfloored@gmail.com>
2022-08-25 11:43:57 +0100

    com

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -12,6 +12,8 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
 
 
 @app.errorhandler(401)
@@ -26,6 +28,16 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
+@app.before_request
+def before_req() -> None:
+    """
+    """
+    if auth is None:
+        return
+    routes_list = ['/api/v1/status/',
+                   '/api/v1/unauthorized/',
+                   '/api/v1/forbidden/']
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
