df27ff32cb52c5d997be2c9b6e395803b7d4abfc
goldfloored <goldfloored@gmail.com>
2022-09-10 10:09:20 +0000

    com

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,21 +1,30 @@
 const http = require('http');
-const dbfile = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 1245;
+const DATABASE = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    await dbfile(process.argv[2])
-      .then((success) => {
-        const output = `This is the list of our students\n${success}`;
-        res.end(output);
-      })
-      .catch((err) => {
-        res.write('This is the list of our students\n');
-        res.end(err.message);
-      });
+    res.write('This is the list of our students\n');
+    try {
+      const { students, csStudents, sweStudents } = await countStudents(DATABASE);
+      res.write(`Number of students: ${students.length}\n`);
+      res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+      res.end();
+    } catch (error) {
+      res.end(error.message);
+    }
+  } else {
+    res.writeHead(404);
+    res.end('Invalid request');
   }
-}).listen(1245);
+});
 
+app.listen(PORT, HOSTNAME);
 module.exports = app;
