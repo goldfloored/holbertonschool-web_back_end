0909f1f477eb43521f310cc3836bdd1eb76c75fd
goldfloored <goldfloored@gmail.com>
2022-09-29 09:20:04 +0000

    com

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
-""" nginx logs in mongoDB """
+"""Log stats of ngnix"""
+
 from pymongo import MongoClient
 
 
-def log_info(log):
-    """ nginx logs in mongoDB """
-    print("{} logs".format(log.estimated_document_count()))
+client = MongoClient()
+
+collection = client.logs.nginx
+
+logs = collection.count_documents({})
+get = collection.count_documents({"method": "GET"})
+post = collection.count_documents({"method": "POST"})
+put = collection.count_documents({"method": "PUT"})
+patch = collection.count_documents({"method": "PATCH"})
+delete = collection.count_documents({"method": "DELETE"})
+status = collection.count_documents({"method": "GET", "path": "/status"})
+
+if __name__ == '__main__':
+    print(f"{logs} logs")
     print("Methods:")
-    print("\tmethod GET: {}".format(log.count_documents({"method": "GET"})))
-    print("\tmethod POST: {}".format(log.count_documents({"method": "POST"})))
-    print("\tmethod PUT: {}".format(log.count_documents({"method": "PUT"})))
-    print("\tmethod PATCH: {}".format(log.count_documents(
-         {"method": "PATCH"})))
-    print("\tmethod DELETE: {}".format(log.count_documents(
-         {"method": "DELETE"})))
-    print("{} status check".format(log.count_documents({"path": "/status"})))
-
-
-if __name__ == "__main__":
-    """ main """
-    c = MongoClient('mongodb://127.0.0.1:27017')
-    log = c.logs.nginx
-    log_info(log)
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{status} status check")
