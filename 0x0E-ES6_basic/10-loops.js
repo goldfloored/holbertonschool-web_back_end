1586ed367b724e0d790fdb4993adc1f9d03539c0
goldfloored <goldfloored@gmail.com>
2022-09-30 10:54:25 +0000

    com

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const idx of array) {
-      array.push(appendString + idx)
-    }
-  
-    return array;
+  for (const idx of array) {
+    array.push(appendString + idx);
   }
+
+  return array;
+}
--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
-    const employee = [];
-    for (const name of Object.keys(report.allEmployees)) {
-      employee.push(...report.allEmployees[name]);
-    }
-    return employee;
+  const employee = [];
+  for (const name of Object.keys(report.allEmployees)) {
+    employee.push(...report.allEmployees[name]);
   }
+  return employee;
+}
